44194a6d9e0f766bf98d03b8fe918d4dad1b8973
masx200 <masx200@qq.com>
2025-03-05 17:01:59 +0800

    build: 更新版本命名策略
    - 在版本号中添加提交哈希值，提高可追溯性
    - 增加错误检查，确保版本号和哈希值不为空
    - 优化脚本，提高构建过程的可靠性

--- build.sh
+++ build.sh
@@ -26,8 +26,28 @@ rm -fvr ./deno-dns-over-https-server-main/static/node_modules/
 rm -fvr ./deno-dns-over-https-server-main/node_modules/
 rm -fvr ./deno-dns-over-https-server-main/deno.lock
 VER_LATEST1=$(curl -fsSL "https://api.github.com/repos/masx200/deno-dns-over-https-server/releases/latest" | jq -r '.tag_name')
+SHA_LATEST1=$(curl -fsSL "https://api.github.com/repos/masx200/deno-dns-over-https-server/commits/main" | jq -r '.sha')
+
 VER_LATEST2=$(curl -fsSL "https://api.github.com/repos/masx200/deno-ddns-over-https-client/releases/latest" | jq -r '.tag_name')
-version="$VER_LATEST1-$VER_LATEST2"
+
+SHA_LATEST2=$(curl -fsSL "https://api.github.com/repos/masx200/deno-ddns-over-https-client/commits/main" | jq -r '.sha')
+if [ -z "$VER_LATEST1" ]; then
+    echo "Error: VER_LATEST1 is empty"
+    exit 1
+fi
+if [ -z "$SHA_LATEST1" ]; then
+    echo "Error: SHA_LATEST1 is empty"
+    exit 1
+fi
+if [ -z "$VER_LATEST2" ]; then
+    echo "Error: VER_LATEST2 is empty"
+    exit 1
+fi
+if [ -z "$SHA_LATEST2" ]; then
+    echo "Error: SHA_LATEST2 is empty"
+    exit 1
+fi
+version="$VER_LATEST1-$VER_LATEST2-$SHA_LATEST1-$SHA_LATEST2"
 echo "version=$version"
 docker build -t "masx200/deno-ddns-over-https-client-deno-dns-over-https-server:temp" .
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock masx200/docker-squash:1.2.2 docker-squash -v -t "masx200/deno-ddns-over-https-client-deno-dns-over-https-server:$version" "masx200/deno-ddns-over-https-client-deno-dns-over-https-server:temp"
